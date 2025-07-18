832b748f69db00be6cf561b86248c2daa0783a75
UmarulFarook <farookrasheed999@gmail.com>
2025-07-18 11:19:18 +0530

    Updated Source Code!

--- src/Components/Banner2/Banner2.js
+++ src/Components/Banner2/Banner2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Banner2.css';
+import posterImage from '../../assets/poster.png'
 
 function Banner2() {
   return (
@@ -7,40 +8,13 @@ function Banner2() {
       <div className="container mt-4 mb-5">
         <div className="row align-items-center">
 
-          <div className="col-lg-6 position-relative ps-5 mb-4">
-            <div className="about-image-grid">
-              <div className="grid-top-left">
-                <img
-                  src="https://media.istockphoto.com/id/1134018566/photo/man-having-a-business-meeting-and-signing-a-contract-recruitment-or-agreement.jpg?s=612x612&w=0&k=20&c=qZGuRs2OZQ6ALC6eHNPmOEOZTV_TOXHLSmJhFJhAfug="
-                  alt="Top Left"
-                  className="img-fluid rounded shadow"
-                />
-              </div>
-              <div className="grid-top-right">
-                <img
-                  src="https://img.freepik.com/free-photo/business-man-owner-company-office_1303-16046.jpg?semt=ais_hybrid&w=740"
-                  alt="Top Right"
-                  className="img-fluid rounded shadow"
-                />
-              </div>
-              <div className="grid-bottom-right">
-                <img
-                  src="https://plus.unsplash.com/premium_photo-1682432982609-578969ff8b75?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8b2ZmaWNlJTIwbWFufGVufDB8fDB8fHww"
-                  alt="Bottom Right"
-                  className="img-fluid rounded shadow"
-                />
-              </div>
-
-              {/* Green Experience Box */}
-              <div className="experience-box">
-                <h3>10+</h3>
-                <small>Years Of<br />Experience</small>
-              </div>
+          <div className="col-lg-6 position-relative ps-3 mb-4">
+            <div className='imgBanner2'>
+              <img src={posterImage} alt=''></img>
             </div>
           </div>
 
-          <div className="col-lg-6 ps-4">
-            <br />
+          <div className="col-lg-6 ps-4 mb-4">
             <p
               className="about text-uppercase small text-start text-success fw-semibold mb-2">
               About Us <i className="bi bi-cursor-fill ms-1"></i>
