cf7d6b5c8516adb367b705ad36e1f67a76a6ca36
UmarulFarook <farookrasheed999@gmail.com>
2025-07-18 12:19:43 +0530

    Updated Mobile responsive

--- src/Components/Banner2/Banner2.js
+++ src/Components/Banner2/Banner2.js
@@ -5,45 +5,48 @@ import posterImage from '../../assets/poster.png'
 function Banner2() {
   return (
     <div id="about" className="about-us-section ">
-      <div className="container mt-5 mb-5">
+      <div className="container mt-5 mb-4">
         <div className="row align-items-center">
 
-          <div className="col-lg-6 position-relative d-flex ps-5 justify-content-start mb-4">
+          <div className="col-lg-6 position-relative d-flex justify-content-center justify-content-lg-start  mb-5">
             <div className='imgBanner2'>
               <img src={posterImage} alt=''></img>
             </div>
           </div>
 
-          <div className="col-lg-6 mb-4">
+          <div className="col-lg-6 mb-4 ">
             <p
-              className="about text-uppercase small text-start text-success fw-semibold mb-2">
+              className="about text-uppercase small text-center text-lg-start text-success fw-semibold mb-2">
               About Us <i className="bi bi-cursor-fill ms-1"></i>
             </p>
-            <h1 className="fw-bold mb-4 mt-2 text-start">
+            <h1 className="fw-bold mb-4 mt-2 text-center text-lg-start">
               Unknown Wanderlust<br />Your Journey into
             </h1>
-            <p className="text-muted mb-4 text-start">
+            <p className="text-muted mb-4 text-center text-lg-start">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit amet rcus nunc. Duis egestas ac ante sed tincidunt.
             </p>
             <div className="row">
-              <div className="col-md-6 mb-3 d-flex justify-content-center text-start">
-                <i className="bi bi-check-square-fill green-icon me-2"></i>
+              <div className="col-md-6 mb-3 d-flex justify-content-center text-center text-lg-start">
+                
                 <div>
-                  <strong>Safety Guides</strong><br />
+                  <strong><i className="bi bi-check-square-fill green-icon me-2"></i>Safety Guides</strong><br />
+                  <div className='ps-lg-4'>
                   <small className="text-muted">Consectetur adipiscing elit text-start text-start  adipiscing elit</small>
+                  </div>
                 </div>
               </div>
-              <div className="col-md-6 mb-3 d-flex justify-content-center text-start">
-                <i className="bi bi-check-square-fill green-icon me-2 "></i>
+              <div className="col-md-6 mb-3 d-flex justify-content-center text-center text-lg-start">
                 <div>
-                  <strong>Passport Assistance</strong><br />
+                  <strong><i className="bi bi-check-square-fill green-icon me-2"></i>Passport Assistance</strong><br />
+                  <div className='ps-lg-4'>
                   <small className="text-muted">Consectetur adipiscing elit text-start text-start  adipiscing elit</small>
+                  </div>
                 </div>
               </div>
             </div>
             <a
               href=""
-              className="aboutbtn btn float-start rounded-pill mt-4"
+              className="aboutbtn btn float-center float-lg-start rounded-pill mt-4"
             >
               Read More <i className="fa fa-arrow-right ms-1"></i>
             </a>
